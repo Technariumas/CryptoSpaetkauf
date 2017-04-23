3157be19a0e30d1da77ff84dd8940572dd933098
opit <opit@hardcore.lt>
2017-04-23 17:20:15 +0300

    contract expiry

--- Bonka.sol
+++ Bonka.sol
@@ -32,23 +32,41 @@ contract bonkaContract is mortal {
         if (msg.value < bonkaPrice+transportFee) throw;
         borrowerWalletAddress = msg.sender;
         start = block.timestamp;
+        status = "borrowing"
         return (new TransportContract).value(transportFee) (lenderCoords, borrowerCoords, this.address);
     }	
-    
 
 	function transportDone() {
-			
+			if (status == "borrowing")
+				lenderWalletAddress.send(bonkaPrice);
+				status = "returning";
+			if (status == "returning")
+				borrowerWalletAddress.send(bonkaPrice);
+				status = "borrowing";
 	}
-
-	function expire(uint start, uint daysAfter) {
-		if (now >= start + 1 days) { ... }
+    
+    
+    
+    function commitToReturn(string lenderCoords, uint transportFee)
+    public
+    payable
+    returns (address)
+    {
+        if (msg.value < bonkaPrice+transportFee) throw;
+        lenderWalletAddress = msg.sender;
+        status = "returning"
+        return (new TransportContract).value(transportFee) (borrowerCoords, lenderCoords, this.address);
+    }
+	
+	
+	function failedToReturn(uint start) {
+		if (now >= start + 1 days) {
+			bonkaContract.kill.sendTransaction()
+		}
 	}	
 
 }
    
-    
-    
-
 contract TransportContract {
     
     function TransportContract(lenderCoords, borrowerCoords, creatorAddress)
@@ -59,13 +77,8 @@ contract TransportContract {
         creatorAddress = creatorAddress;
     }
     
-    
-    
-    //creator
-    
     function commitToTransport(string lenderCoords, string borrowerCoords, this.address) 
     public
-    payable
     {
         /* commit fee must be at least equal to the bottle price*/
 	if (msg.value < bonkaPrice+transportFee) throw;
